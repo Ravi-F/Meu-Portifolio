7faa94d487e451564636a1d0f2ea74ba39b7c187
Ravi <ravifrei1@gmail.com>
2025-02-07 19:01:27 -0300

    Navbar e Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import { LoadingScreen } from "./components/loadingscreen";
-
+import { Navbar } from "./components/navbar";
+import { MobileMenu } from "./components/MobileMenu";
+import { Home } from "./components/sections/home";
 import "./index.css";
 
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
-
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
@@ -16,6 +18,9 @@ function App() {
           isLoaded ? "opacity-100" : "opacity-0"
         } bg-black text-gray-100`}
       >
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        < Home />
       </div>
     </>
   );
---
+++ src/components/sections/home.jsx
@@ -0,0 +1,36 @@
+export const Home = () => {
+  return (
+    <section
+      id="home"
+      className="min-h-screen flex items-center justify-center relative"
+    >
+      <div className="text-center z-10 p-4">
+        <h1 className="text-5xl md:text-7x1 font-bold mb-6 bg-gradient-to-r from-green-500 to-yellow-600 bg-clip-text text-transparent leading-right">
+          Olá, eu sou o Ravi
+        </h1>
+        <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
+          Sou um desenvolvedor web apaixonado por tecnologia e inovação. Estou
+          sempre procurando por novas maneiras de melhorar minhas habilidades e
+          aprender com as pessoas ao meu redor.
+        </p>
+        <div className="flex justify-center space-x-4">
+          <a
+            href="#projetos"
+            className="bg-green-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
+          >
+            Meus Projetos
+          </a>
+
+          <a
+            href="#contatos"
+            className="border border-green-500/50 text-green-500 py-3 px-6 rounded font-medium transition-all duration-200 
+             hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-green-500/10"
+          >
+            Entre em contato
+          </a>
+          
+        </div>
+      </div>
+    </section>
+  );
+};
